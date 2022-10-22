0c4a4d0405ff213ab1f2df43f69cf5c3826dc77e
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-22 23:31:58 +0600

    data loaded from server site

--- src/layouts/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/layouts/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories, setCategoies]= useState([])
+    
+    useEffect(() =>{
+        fetch('http://localhost:5000/news-categories')
+        .then(res => res.json())
+        .then(data => setCategoies(data))
+    })
     return (
         <div>
-            <h2>Left Side</h2>
+            <h2>All Category</h2>
+            {
+                categories.map(category =><p key={category.id}>
+                    <Link to={`/category/${category.id}`}>{category.name}</Link>
+                </p>)
+            }
         </div>
     );
 };
