f0d866cad318186722c31e33f70f8e5845473d5b
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-22 22:34:18 +0600

    Components added

--- src/Routes/Routes/Route.js
+++ src/Routes/Routes/Route.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
+import Category from "../../layouts/Pages/Catagory/Category";
+import Home from "../../layouts/Pages/Home/Home";
+import News from "../../layouts/Pages/News/News";
 
 export const routes=createBrowserRouter([
 {
@@ -7,10 +10,16 @@ export const routes=createBrowserRouter([
     element: <Main></Main>,
     children: [
         {
-
+            path:'/',
+            element: <Home></Home>
         },
         {
-            
+            path: '/category/:id',
+            element:<Category></Category>
+        }
+       , {
+            path: '/news/:id',
+            element:<News></News>
         }
     ]
 }
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
+import Footer from './Pages/Shared/Footer';
+import Header from './Pages/Shared/Header/Header';
+import LeftSideNav from './Pages/Shared/LeftSideNav/LeftSideNav';
+import RightSideNav from './Pages/Shared/RightSideNav/RightSideNav';
 
 const Main = () => {
     return (
         <div>
-          <Outlet></Outlet>  
+            <Header></Header>
+          <Container>
+           <Row>
+           <Col lg='2'>
+            <RightSideNav></RightSideNav>
+            </Col>
+            <Col lg='7'>
+            <Outlet></Outlet>
+            </Col>
+            <Col lg='3'>
+                <LeftSideNav></LeftSideNav>
+            </Col>
+           </Row>
+          </Container>
+          <Footer></Footer>
         </div>
     );
 };
