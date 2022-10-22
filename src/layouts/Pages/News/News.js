4766840d54ab3d8c8ed5b7b7720f8083a15080ce
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-23 03:06:32 +0600

    news Cart added

--- src/layouts/Pages/News/News.js
+++ src/layouts/Pages/News/News.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
+
 const News = () => {
     const news= useLoaderData()
+    
     return (
-        <div>
-            <h1>News</h1>
-        </div>
+       <div></div>
     );
 };
 
